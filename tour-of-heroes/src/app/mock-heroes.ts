48dff9deec90457cd454b4aedac5c7b7a8c5074b
Yamin Bin Yahiya <yaminbyahiya@gmail.com>
2023-05-24 11:58:45 +0600

    ngFor Directive to List Items

---
+++ tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import { Hero } from "./hero";
+export const HEROES:Hero[]=[
+    { id: 1, name: 'Iron Man' },
+    { id: 13, name: 'Spider Man' },
+    { id: 14, name: 'Black Panther' },
+    { id: 15, name: 'Hulk' },
+    { id: 16, name: 'Wolverine' },
+    { id: 17, name: 'DareDevil' },
+    { id: 18, name: 'Punisher' },
+    { id: 19, name: 'Doctor Strange' },
+    { id: 20, name: 'Thor' }
+]
